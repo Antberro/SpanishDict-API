51b5f87652605c9efb93a88de301a4a367a2c10c
Antonio Berrones <antoniob@mit.edu>
2021-05-31 22:57:12 -0400

    write skeleton code

---
+++ SpanishDictClient.py
@@ -0,0 +1,18 @@
+class SpanishDictClient(object):
+    def __init__(self):
+        ''''
+        Write description here.
+        '''
+        pass
+
+    def translate(self, text: str) -> str:
+        '''
+        Write description here.
+
+        Args:
+            text (str): text to translate
+
+        Returns:
+            message (str): translated text
+        '''
+        pass
